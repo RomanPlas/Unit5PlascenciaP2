35bd5d1b9af7cdb414ecb2f433867c703be9a08b
Roman Plascencia <705392@student.dusd.net>
2023-04-19 09:55:30 -0700

    Finished 5.2 and 5.3
    Need to do 5.4 and bonus

--- Unit5PlascenciaP2/Assets/Course Library/Scripts/Target.cs
+++ Unit5PlascenciaP2/Assets/Course Library/Scripts/Target.cs
@@ -11,6 +11,8 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -2;
     private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
     
 
     // Start is called before the first frame update
@@ -46,11 +48,24 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+
+            Instantiate(explosionParticle, transform.position,
+            explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
+
+        
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 }
